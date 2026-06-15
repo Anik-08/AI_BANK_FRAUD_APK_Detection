d3eb49871c75bee71442881acb29ae79e14291e0
Anik-08 <anik.tiwary08@gmail.com>
2026-06-15 16:19:11 +0530

    updated build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     proxy: {
       '/api': {
@@ -12,5 +12,10 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: true,
   },
 })
